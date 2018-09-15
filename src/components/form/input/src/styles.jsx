57f7bcbe83af5f39db5d230f58d35b2f51d8e9ff
Victor Ribero <victor.ribero3@gmail.com>
2018-09-15 19:23:43 +0200

    Versions update and Form/input updates

--- src/components/form/input/src/styles.jsx
+++ src/components/form/input/src/styles.jsx
@@ -13,11 +13,12 @@ const InputStyled = styled.input`
   box-shadow: 0 6px 8px rgba(102, 119, 136, 0.03),
     0 1px 2px rgba(102, 119, 136, 0.3);
   background-color: ${props => props.theme.colors.grey_light};
-  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "initial")}
-  opacity: ${({ disabled }) => (disabled ? 0.4 : 1)}
+  ${({ shape }) => stylesShape(shape)};
+  ${({ disabled }) => stylesDisabled(disabled)};
+  ${({ size, theme }) => sylesSize(size, theme)};
 
   &:focus {
-    background: ${props => darken(0.15, props.theme.colors.grey_light)};
+    background: ${props => darken(0.1, props.theme.colors.grey_light)};
   }
 
   & + label {
@@ -33,42 +34,51 @@ const InputStyled = styled.input`
         }
       `
       : ``};
-
-  ${({ shape }) =>
-    shape === "square"
-      ? `border-radius: 0;`
-      : shape === "rounded"
-        ? `border-radius: 2px`
-        : shape === "pill"
-          ? `border-radius: 500px`
-          : ``};
-
-  ${({ size, theme }) =>
-    size === "small"
-      ? `
-        font-size: ${theme.font.size.xs};
-        padding-top: ${theme.spacing.padd.s};
-        padding-bottom: ${theme.spacing.padd.s};
-        padding-right: ${theme.spacing.padd.s};
-        padding-left: ${theme.spacing.padd.s};
-      `
-      : size === "medium"
-        ? `
-        font-size: ${theme.font.size.s};
-        padding-top: ${theme.spacing.padd.m};
-        padding-bottom: ${theme.spacing.padd.m};
-        padding-right: ${theme.spacing.padd.m};
-        padding-left: ${theme.spacing.padd.m};
-      `
-        : size === "large"
-          ? `
-        font-size: ${theme.font.size.m};
-        padding-top: ${theme.spacing.padd.l};
-        padding-bottom: ${theme.spacing.padd.l};
-        padding-right: ${theme.spacing.padd.l};
-        padding-left: ${theme.spacing.padd.l};
-      `
-          : ``};
 `;
 
+const stylesShape = shape =>
+  shape === "square"
+    ? `border-radius: 0;`
+    : shape === "rounded"
+      ? `border-radius: 3px`
+      : shape === "pill"
+        ? `border-radius: 500px`
+        : ``;
+
+const sylesSize = (size, theme) =>
+  size === "small"
+    ? `
+    font-size: ${theme.font.size.xs};
+    padding-top: ${theme.spacing.padd.s};
+    padding-bottom: ${theme.spacing.padd.s};
+    padding-right: ${theme.spacing.padd.s};
+    padding-left: ${theme.spacing.padd.s};
+  `
+    : size === "medium"
+      ? `
+    font-size: ${theme.font.size.s};
+    padding-top: ${theme.spacing.padd.m};
+    padding-bottom: ${theme.spacing.padd.m};
+    padding-right: ${theme.spacing.padd.m};
+    padding-left: ${theme.spacing.padd.m};
+  `
+      : size === "large"
+        ? `
+    font-size: ${theme.font.size.m};
+    padding-top: ${theme.spacing.padd.l};
+    padding-bottom: ${theme.spacing.padd.l};
+    padding-right: ${theme.spacing.padd.l};
+    padding-left: ${theme.spacing.padd.l};
+  `
+        : ``;
+
+const stylesDisabled = disabled =>
+  disabled
+    ? `
+      cursor: not-allowed;
+      opacity: 0.4;`
+    : `
+      cursor: text;
+      opacity: 1;
+    `;
 export { InputStyled };

b94ffde5d4c6f45d1539a802f3425321bb6f4359
Victor Ribero <victor.ribero3@gmail.com>
2018-09-15 19:03:54 +0200

    Avatar/basic giant size added in

--- src/components/avatar/basic/src/styles.jsx
+++ src/components/avatar/basic/src/styles.jsx
@@ -1,55 +1,56 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const AvatarBasicStyled = styled.img`
-  border: ${props => props.isBordered ? '1px solid' : 'none'};
+  border: ${props => (props.isBordered ? "1px solid" : "none")};
   outline: none;
   box-sizing: border-box;
   display: inline-block;
-  transition: .15s ease all;
+  transition: 0.15s ease all;
 
   ${({ size }) =>
-    size === 'xsmall'
+    size === "xsmall"
       ? `
     width: 1.5rem;
     height: 1.5rem;
     `
-      : size === 'small'
+      : size === "small"
         ? `
         width: 2rem;
         height: 2rem;
         `
-        : size === 'medium'
+        : size === "medium"
           ? `
             width: 2.5rem;
             height: 2.5rem;
             `
-          : size === 'large'
+          : size === "large"
             ? `
             width: 3.5rem;
             height: 3.5rem;
-            `: ``
-  }
-
-  ${({ shape }) =>
-    shape === 'square'
+          `
+            : size === "giant"
+              ? `
+            width: 10rem;
+            height: 10rem;
+            `
+              : ``} ${({ shape }) =>
+    shape === "square"
       ? `border-radius: 0;`
-      : shape === 'rounded'
+      : shape === "rounded"
         ? `border-radius: 3px`
-        : shape === 'circle'
-          ? `border-radius: 500px` : ``
-  };
+        : shape === "circle"
+          ? `border-radius: 500px`
+          : ``};
 
   ${({ borderColor, theme }) =>
-    borderColor === 'primary'
-      ? `border-color: ${theme.colors.primary};` :
-      borderColor === 'secondary'
-        ? `border-color: ${theme.colors.accent};` :
-        borderColor === 'system'
-          ? `border-color: ${theme.colors.system};` :
-          borderColor === 'grey'
-            ? `border-color: ${theme.colors.grey};` : ``
-  };
-`
-export {
-  AvatarBasicStyled
-}
+    borderColor === "primary"
+      ? `border-color: ${theme.colors.primary};`
+      : borderColor === "secondary"
+        ? `border-color: ${theme.colors.accent};`
+        : borderColor === "system"
+          ? `border-color: ${theme.colors.system};`
+          : borderColor === "grey"
+            ? `border-color: ${theme.colors.grey};`
+            : ``};
+`;
+export { AvatarBasicStyled };

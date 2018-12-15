039882d9479ce703966c4fb2d886faa9541a920d
Victor Ribero <victor.ribero3@gmail.com>
2018-12-15 21:01:55 +0100

    Button version 2.0.9

--- src/components/button/basic/src/index.jsx
+++ src/components/button/basic/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ButtonBasicStyled, LinkStyled, ButtonInnerStyled } from "./styles";
 
-export const ButtonBasic = ({
+const ButtonBasic = ({
   size,
   type,
   color,
@@ -87,3 +87,5 @@ ButtonBasic.defaultProps = {
   isElevated: true,
   fullWidth: false
 };
+
+export default ButtonBasic;

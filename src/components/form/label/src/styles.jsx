6f709e4cd906d096d34b2e83d22ba2a2b69d30ea
Victor Ribero <victor.ribero3@gmail.com>
2018-12-19 23:22:03 +0100

    version 3.0.1 Label. Reduced margin-bottom

--- src/components/form/label/src/styles.jsx
+++ src/components/form/label/src/styles.jsx
@@ -4,10 +4,10 @@ const LabelStyled = styled.label`
   display: block;
   color: ${props => props.theme.palette.grey["900"]};
   font-size: ${props => props.theme.font.size.m};
-  font-family: ${props => props.theme.font.family.main};
+  font-family: ${props => props.theme.font.family.primary};
   font-weight: ${props => props.theme.font.weight.m};
   letter-spacing: 0.25px;
-  margin-bottom: ${props => props.theme.spacing.xs};
+  margin-bottom: ${props => props.theme.spacing.xxs};
 `;
 
 export { LabelStyled };

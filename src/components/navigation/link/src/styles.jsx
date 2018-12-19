e4a52e8ea4da9a17d3118c1129961a0291b09d9a
Victor Ribero <victor.ribero3@gmail.com>
2018-12-19 21:26:15 +0100

    Version of 3...4

--- src/components/navigation/link/src/styles.jsx
+++ src/components/navigation/link/src/styles.jsx
@@ -1,16 +1,16 @@
 import styled from "@emotion/styled";
 
 const LinkStyled = styled.a`
-  font-family: ${props => props.theme.font.family.main};
-  font-weight: ${props => props.theme.font.weight.m};
-  color: ${theme.palette.primary["500"]}
+  font-family: ${({ theme }) => theme.font.family.main};
+  font-weight: ${({ theme }) => theme.font.weight.m};
+  color: ${({ theme }) => theme.palette.primary["500"]}
   text-decoration: ${({ isUnderlined }) =>
     isUnderlined ? "underline" : "none"};
   cursor: pointer;
   transition: text-decoration 0.15s ease;
 
   :hover {
-    color: ${theme.palette.primary["700"]}
+    color: ${({ theme }) => theme.palette.primary["700"]}
     text-decoration: underline;
   }
 `;

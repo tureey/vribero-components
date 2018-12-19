a5f081b5335227aef709f400a1cb6672caecb583
Victor Ribero <victor.ribero3@gmail.com>
2018-12-19 21:32:29 +0100

    version 3.0.5

--- src/components/navigation/link/src/styles.jsx
+++ src/components/navigation/link/src/styles.jsx
@@ -3,14 +3,14 @@ import styled from "@emotion/styled";
 const LinkStyled = styled.a`
   font-family: ${({ theme }) => theme.font.family.main};
   font-weight: ${({ theme }) => theme.font.weight.m};
-  color: ${({ theme }) => theme.palette.primary["500"]}
+  color: ${({ theme }) => theme.palette.primary["500"]};
   text-decoration: ${({ isUnderlined }) =>
     isUnderlined ? "underline" : "none"};
   cursor: pointer;
   transition: text-decoration 0.15s ease;
 
   :hover {
-    color: ${({ theme }) => theme.palette.primary["700"]}
+    color: ${({ theme }) => theme.palette.primary["700"]};
     text-decoration: underline;
   }
 `;

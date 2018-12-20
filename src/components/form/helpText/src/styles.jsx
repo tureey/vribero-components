aa33c48a9880fd12f22a54dca8e0b01597511034
Victor Ribero <victor.ribero3@gmail.com>
2018-12-20 10:50:59 +0100

    Version 3.0.1 HelpTExt

--- src/components/form/helpText/src/styles.jsx
+++ src/components/form/helpText/src/styles.jsx
@@ -4,7 +4,7 @@ const HelpTextStyled = styled.span`
   display: block;
   color: ${({ theme }) => theme.palette.grey["700"]};
   font-size: ${props => props.theme.font.size.xs};
-  font-family: ${props => props.theme.font.family.main};
+  font-family: ${props => props.theme.font.family.primary};
   font-weight: ${props => props.theme.font.weight.s};
   letter-spacing: 0.25px;
   margin-top: ${props => props.theme.spacing.xs};

69d24da6a906e1f4d76cb5c680fd9f8d9dbeccb6
Victor Ribero <victor.ribero3@gmail.com>
2018-12-22 11:10:22 +0100

    Version 3.1.0 label

--- src/components/form/label/src/styles.jsx
+++ src/components/form/label/src/styles.jsx
@@ -2,10 +2,10 @@ import styled from "@emotion/styled";
 
 const LabelStyled = styled.label`
   display: block;
-  color: ${props => props.theme.palette.grey["900"]};
+  color: ${props => props.theme.palette.grey["700"]};
   font-size: ${props => props.theme.font.size.m};
   font-family: ${props => props.theme.font.family.primary};
-  font-weight: ${props => props.theme.font.weight.m};
+  font-weight: ${props => props.theme.font.weight.medium};
   letter-spacing: 0.25px;
   margin-bottom: ${props => props.theme.spacing.xxs};
 `;

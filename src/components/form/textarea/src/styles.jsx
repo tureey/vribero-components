9e27cc1bd9e32f5bc2f93dfd0c1b962e5d3878ed
Victor Ribero <victor.ribero3@gmail.com>
2019-01-23 19:31:53 +0100

    Version 3.1.0 Form/Textarea (BackgroundColor less grey)

--- src/components/form/textarea/src/styles.jsx
+++ src/components/form/textarea/src/styles.jsx
@@ -15,9 +15,9 @@ const TextareaStyled = styled.textarea`
   font-weight: 300;
   box-shadow: 0 6px 8px rgba(102, 119, 136, 0.03),
     0 1px 2px rgba(102, 119, 136, 0.3);
-  background-color: ${props => props.theme.palette.grey["200"]};
+  background-color: ${props => props.theme.palette.grey["100"]};
   &:focus {
-    background: ${props => props.theme.palette.grey["300"]};
+    background: ${props => props.theme.palette.grey["200"]};
   }
 
   & + label {

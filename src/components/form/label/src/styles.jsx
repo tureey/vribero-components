ca1d0173d6ac916f651abe8ca20786060b0b80d4
Victor Ribero <victor.ribero3@gmail.com>
2019-01-23 21:37:25 +0100

    Version 3.2.0 Form/Label

--- src/components/form/label/src/styles.jsx
+++ src/components/form/label/src/styles.jsx
@@ -2,12 +2,11 @@ import styled from "@emotion/styled";
 
 const LabelStyled = styled.label`
   display: block;
-  color: ${props => props.theme.palette.grey["700"]};
+  color: ${props => props.theme.palette.grey["800"]};
   font-size: ${props => props.theme.font.size.m};
   font-family: ${props => props.theme.font.family.primary};
   font-weight: ${props => props.theme.font.weight.medium};
   letter-spacing: 0.25px;
-  margin-bottom: ${props => props.theme.spacing.xxs};
 `;
 
 export { LabelStyled };

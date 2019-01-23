a215987480539691b642f4f0cdec3738b0878ced
Victor Ribero <victor.ribero3@gmail.com>
2019-01-23 22:01:03 +0100

    Version 3.2.0 form/helpText Added some spacing

--- src/components/form/helpText/src/index.jsx
+++ src/components/form/helpText/src/index.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { HelpTextStyled } from './styles'
+import React from "react";
+import PropTypes from "prop-types";
+import { HelpTextStyled } from "./styles";
 
 const HelpText = props => (
-  <HelpTextStyled {...props}>
-    {props.children}
-  </HelpTextStyled>
-)
+  <HelpTextStyled {...props}>{props.children}</HelpTextStyled>
+);
 
 HelpText.propTypes = {
-  theme: PropTypes.object,
-}
+  theme: PropTypes.object
+};
 
-export default HelpText
+export default HelpText;
--- src/components/form/helpText/src/styles.jsx
+++ src/components/form/helpText/src/styles.jsx
@@ -2,14 +2,19 @@ import styled from "@emotion/styled";
 
 const HelpTextStyled = styled.span`
   display: block;
+  margin-top: ${props => props.theme.spacing.xxs};
   color: ${({ theme }) => theme.palette.grey["700"]};
   font-size: ${props => props.theme.font.size.xs};
   font-family: ${props => props.theme.font.family.primary};
   font-weight: ${props => props.theme.font.weight.light};
-  letter-spacing: 0.25px;
-  margin-top: ${props => props.theme.spacing.xs};
   font-style: italic;
-  margin-bottom: ${props => props.theme.spacing.xs};
+  letter-spacing: 0.25px;
+
+  + input,
+  + textarea,
+  + select {
+    margin-top: ${props => props.theme.spacing.xs};
+  }
 `;
 
 export { HelpTextStyled };

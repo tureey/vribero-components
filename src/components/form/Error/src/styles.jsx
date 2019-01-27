997c6546dca53b884c6c0debc3a7bfd51af2ceb2
Victor Ribero <victor.ribero3@gmail.com>
2019-01-27 10:24:26 +0100

    Form/Error Changes

--- src/components/form/Error/src/styles.jsx
+++ src/components/form/Error/src/styles.jsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-const Text = styled.input`
+const Text = styled.span`
   color: ${props => props.theme.palette.accent.red["500"]};
   font-size: ${props => props.theme.font.size.s};
   margin-top: ${props => props.theme.spacing.xs};

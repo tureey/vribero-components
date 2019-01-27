b8db11b681e635db0922e7d131153e2ad5ce8bc2
Victor Ribero <victor.ribero3@gmail.com>
2019-01-27 10:28:32 +0100

    Version 1.1.0 (form/error) Added display: block

--- src/components/form/Error/src/styles.jsx
+++ src/components/form/Error/src/styles.jsx
@@ -4,6 +4,7 @@ const Text = styled.span`
   color: ${props => props.theme.palette.accent.red["500"]};
   font-size: ${props => props.theme.font.size.s};
   margin-top: ${props => props.theme.spacing.xs};
+  display: block;
 `;
 
 export { Text };
